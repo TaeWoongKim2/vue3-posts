c300847cf24a83549f00c234ee5cff9cf54c097f
TaeWoongKim2 <ktaewoong46@gmail.com>
2023-05-12 00:01:19 +0900

    setup: for follow the lecture

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+	<h2>About View</h2>
+	{{ $route.path }}
+	<button type="button" @click="$router.push('/')">Go to Home</button>
+	<button type="button" @click="handleClickHome">Go to Home</button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const handleClickHome = () => {
+	router.push('/');
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+	<h2>Home View</h2>
+	{{ $route.path }}
+	<button type="button" class="btn btn-primary" @click="$router.push('/about')">
+		Go to About
+	</button>
+	<button type="button" class="btn btn-primary" @click="handleClickAbout">
+		Go to About
+	</button>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+console.log('[route.path]:', route.path);
+
+const router = useRouter();
+
+const handleClickAbout = () => {
+	router.push('/about');
+};
+</script>
+
+<style lang="scss" scoped></style>
